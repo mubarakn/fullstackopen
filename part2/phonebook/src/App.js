4a00337ab1c12c04b0f02df56e692c2b92717218
Mubarak Basha <mubarak.tgi@gmail.com>
2021-11-30 14:38:21 +0530

    2.11 The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 const Filter = ({ query, onQueryChange }) => {
   return (
@@ -30,15 +31,20 @@ const Persons = ({ persons, query }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        const { data } = response
+        console.log(data)
+        setPersons(data)
+      })
+  }, [])
 
   const handleFormSubmit = e => {
     e.preventDefault()
